ab697c62fd23db83f58c5bc2ca416467c3654be8
Вадим Мотов <motovu2@tk34.mskobr.ru>
2021-11-11 13:55:44 +0300

    Добавьте файлы проекта.

---
+++ Diagaramm/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Diagaramm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            chart.ChartAreas.Add(new ChartArea("Default"));
+
+            // Добавим линию, и назначим ее в ранее созданную область "Default"
+            chart.Series.Add(new Series("Series1"));
+            chart.Series["Series1"].ChartArea = "Default";
+            chart.Series["Series1"].ChartType = SeriesChartType.Line;
+
+            // добавим данные линии
+            string[] axisXData = new string[] { "a", "b", "c" };
+            double[] axisYData = new double[] { 0.5, 1.5, 1.9 };
+            chart.Series["Series1"].Points.DataBindXY(axisXData, axisYData);
+        }
+    }
+}
